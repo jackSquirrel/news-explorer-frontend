d66e602ed0fe00e2fedcfd6b9cda09eac82f0b21
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-15 14:52:56 +0700

    поставил css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -11,6 +12,13 @@ module.exports = {
       test: /\.js$/,
       use: { loader: "babel-loader" },
       exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader']
     }]
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename: 'style.css'})
+  ]
 }

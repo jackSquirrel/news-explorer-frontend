05e4d5743af9424fb7af7aa6685362c68c0a9735
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-15 17:05:44 +0700

    Решены проблемы с версией html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules: [{
@@ -19,6 +21,12 @@ module.exports = {
     }]
   },
   plugins: [
-    new MiniCssExtractPlugin({filename: 'style.css'})
+    new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+    new HtmlWebpackPlugin({
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+    }),
+    new WebpackMd5Hash()
   ]
 }

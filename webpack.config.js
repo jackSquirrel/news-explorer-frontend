994f2720ed129dc9418bd8bc6ab17912c01fd6ae
Timur Ismagilov <ismagilov42@yandex.ru>
2020-04-15 17:30:27 +0700

    установил gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader']
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
     }]
   },
   plugins: [
